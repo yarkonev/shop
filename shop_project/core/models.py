f81254ab0a421a39d3600f6622ec2cbfe3f588d7
amigo <kepler42@skiff.com>
2023-11-20 16:40:18 +0300

    add models: Category, Product

--- shop_project/core/models.py
+++ shop_project/core/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class Category(models.Model):
+    """Категория товара.
+    Содержит название и родительские категории."""
+
+    title = models.CharField(max_length=255)
+    parent = models.ForeignKey("self", on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Product(models.Model):
+    """Товар.
+    Содержит название, категорию, количество и цену."""
+
+    title = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=False)
+    count = models.IntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(100000)]
+    )
+    cost = models.DecimalField(
+        max_digits=10, decimal_places=2, validators=[MinValueValidator(0)]
+    )
+
+    def get_categories_path(self):
+        """
+        Возвращает категорию товара и все родительские категории.
+
+        :return: The path of categories for the current category, joined by " · ".
+        :rtype: str
+        """
+        category_path = [self.category.title]
+        current_category = self.category.parent
+        while current_category is not None:
+            category_path.append(current_category.title)
+            current_category = current_category.parent
+        return " · ".join(reversed(category_path))
+
+    def __str__(self):
+        return self.title
